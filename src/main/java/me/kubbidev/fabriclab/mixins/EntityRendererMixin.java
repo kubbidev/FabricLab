b6e6bff7d500e574ea6e49b4b8c4cf5f7d174603
Kubbi <nicoladelaroche@gmail.com>
2025-09-16 22:31:33 +0200

    fix: Update EntityRendererMixin to target Entity class

--- src/main/java/me/kubbidev/fabriclab/mixins/EntityRendererMixin.java
+++ src/main/java/me/kubbidev/fabriclab/mixins/EntityRendererMixin.java
@@ -1,12 +1,12 @@
 package me.kubbidev.fabriclab.mixins;
 
-import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(EntityRenderer.class)
+@Mixin(Entity.class)
 public class EntityRendererMixin {
 
     @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
